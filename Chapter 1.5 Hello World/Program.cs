e697c3cd964651bc5f29c208afd1b7ecbe60a6c6
Ethan Kovalenko <ashervoronin@gmail.com>
2020-12-16 11:10:34 -0600

    Started C#.

---
+++ Chapter 1.5 Hello World/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chapter_1._5_Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
